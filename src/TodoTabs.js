3a40d710bfc6da164135bb7f0de662104a6c851f
Fredy Izquierdo <fredy_izquierdo@hotmail.es>
2025-01-21 12:58:53 +0100

    feat: Add styles for small displays, for the component TodoTabs

--- src/TodoTabs.js
+++ src/TodoTabs.js
@@ -3,13 +3,13 @@ import './TodoTabs.css';
 function TodoTabs(){
     return (
       <div className='container-tabs'>
-        <div className="todo-tab">
+        <div className="todo-tab tabs">
             <p>To Do</p>
         </div>
-        <div className="doing-tab">
+        <div className="doing-tab tabs">
             <p>Doing</p>
         </div>
-        <div className="done-tab">
+        <div className="done-tab tabs">
             <p>Done</p>
         </div>
       </div>
