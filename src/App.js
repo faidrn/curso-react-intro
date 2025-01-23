d7b28500a2bc839b5b18fe5f1a681e36f09721a7
Fredy Izquierdo <fredy_izquierdo@hotmail.es>
2025-01-23 23:52:52 +0100

    feat: Add events to actions in the web and add states

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ import { TodoListDoing } from './TodoListDoing';
 import { TodoItemDoing } from './TodoItemDoing';
 import { TodoListDone } from './TodoListDone';
 import { TodoItemDone } from './TodoItemDone';
-//import React from 'react';
+import React from 'react';
 
 import './App.css';
 
@@ -18,9 +18,99 @@ const defaultTodos = [
   { text: 'Tomar el curso de Intro a React.js', status: 'doing'},
   { text: 'Llorar con la Llorona', status: 'listed'},
   { text: 'LALALALA', status: 'listed'},
+  { text: 'Usar estados derivados', status: 'done'},
 ];
+/*
+const listTasks = defaultTodos.filter((task) => 
+  task.status === 'listed'
+);
+
+const doingTasks = defaultTodos.filter((task) => 
+  task.status === 'doing'
+);
+
+const doneTasks = defaultTodos.filter((task) => 
+  task.status === 'done'
+);*/
 
 function App() {
+  const [todos, setTodos] = React.useState(defaultTodos);
+  const [searchValue, setSearchValue] = React.useState('');
+
+  const completedTodos = defaultTodos.filter(todo => todo.status === 'done').length;
+  const totalTodos = todos.length; //todos=>const [todos, setTodos] = React.useState(defaultTodos);
+
+  const searchedTodosListed = defaultTodos.filter(
+    (todo) => {
+      const todoText = todo.text.toLowerCase();
+      const searchText = searchValue.toLowerCase();
+      return todoText.includes(searchText) && todo.status === 'listed';
+    }
+  );
+
+  const searchedTodosDoing = defaultTodos.filter(
+    (todo) => {
+      const todoText = todo.text.toLowerCase();
+      const searchText = searchValue.toLowerCase();
+
+      return todoText.includes(searchText) && todo.status === 'doing';
+    }
+  );
+
+  const searchedTodosDone = defaultTodos.filter(
+    (todo) => {
+      const todoText = todo.text.toLowerCase();
+      const searchText = searchValue.toLowerCase();
+
+      return todoText.includes(searchText) && todo.status === 'done';
+    }
+  );
+
+  const completeTodo = (text) => {
+    const newTodos = [...todos]; 
+    const todoIndex = newTodos.findIndex(
+      (todo) => todo.text === text
+    );
+
+    newTodos[todoIndex].status = 'done';
+
+    setTodos(newTodos);
+  };
+
+  const doingTodo = (text) => {
+    const newTodos = [...todos]; 
+    const todoIndex = newTodos.findIndex(
+      (todo) => todo.text === text
+    );
+
+    newTodos[todoIndex].status = 'doing';
+
+    setTodos(newTodos);
+  };
+
+  const returnTodo = (text) => {
+    const newTodos = [...todos]; 
+    const todoIndex = newTodos.findIndex(
+      (todo) => todo.text === text
+    );
+
+    newTodos[todoIndex].status = 'listed';
+
+    setTodos(newTodos);
+  };
+
+  const deleteTodo = (text) => {
+    const newTodos = [...todos]; 
+    const todoIndex = newTodos.findIndex(
+      (todo) => todo.text === text
+    );
+
+    newTodos.splice(todoIndex, 1);
+
+    setTodos(newTodos);
+  };
+
+
   return (
    
    //<React.Fragment> = <>
@@ -28,21 +118,26 @@ function App() {
 
       <TodoUser name={'Fredy Izquierdo'} />
 
-      <TodoCounter completed={16} total={25} />
-      <TodoSearch />
+      <TodoCounter completed={completedTodos} total={totalTodos} />
+      <TodoSearch 
+        searchValue={searchValue} 
+        setSearchValue={setSearchValue}
+      />
 
       <TodoTabs />
       
       <div className='todo-container'> 
         <TodoListAdd>
-          {defaultTodos.map(todo => (
-            <TodoItem 
-              key={todo.text} 
-              text={todo.text} 
-              completed={todo.completed} 
-            />
+          {searchedTodosListed.map(todo => (
+              <TodoItem 
+                key={todo.text} 
+                text={todo.text} 
+                onDoing={() => doingTodo(todo.text)}
+                onDelete={() => deleteTodo(todo.text)}
+              />
+            
           ))}
-
+          
           <CreateTodoButton />
           
         </TodoListAdd>
@@ -50,21 +145,21 @@ function App() {
         
 
         <TodoListDoing>
-          {defaultTodos.map(todo => (
+          {searchedTodosDoing.map(todo => (
             <TodoItemDoing 
               key={todo.text} 
               text={todo.text} 
-              completed={todo.completed} 
+              onComplete={() => completeTodo(todo.text)}
+              onTodo={() => returnTodo(todo.text)}
             />
           ))}
         </TodoListDoing>
 
         <TodoListDone>
-          {defaultTodos.map(todo => (
+          {searchedTodosDone.map(todo => (
             <TodoItemDone 
               key={todo.text} 
-              text={todo.text} 
-              completed={todo.completed}             
+              text={todo.text}            
             />
           ))}
         </TodoListDone>       
