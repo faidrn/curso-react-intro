9e10d9f6b4c32ac3eb38cfae22857256bbeb68a5
Fredy Izquierdo <fredy_izquierdo@hotmail.es>
2025-01-24 23:47:31 +0100

    feat: Add congratulatory message Completed ToDos

--- src/TodoCounter.js
+++ src/TodoCounter.js
@@ -3,7 +3,15 @@ import './TodoCounter.css';
 function TodoCounter({ total, completed }){
     return (
       <h1 className='TodoCounter'>
-        Has completado <span>{completed}</span> de <span>{total}</span> TODOs
+        {total > completed ? (
+          <>
+            Has completado <span>{completed}</span> de <span>{total}</span> TODOs
+          </>
+        ) : (
+          <>
+            <span>¡Felicitaciones!</span> Has completado todos los TODOs
+          </>
+        )}
       </h1>
     );
   }
